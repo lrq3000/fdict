bfac4f9be6048d276a315e0491f6793b6d286ea0
Stephen L <lrq3000@gmail.com>
2017-08-08 17:06:11 +0200

    bump version 0.7.8
    Signed-off-by: Stephen L. <lrq3000@gmail.com>

--- fdict/_version.py
+++ fdict/_version.py
@@ -3,7 +3,7 @@
 __all__ = ["__version__"]
 
 # major, minor, patch, -extra
-version_info = 0, 7, 7
+version_info = 0, 7, 8
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
